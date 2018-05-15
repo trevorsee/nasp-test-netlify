1aff4436f4890e64b0a594e29812be1339251efa
trevorsee <tcarr@mica.edu>
2018-05-14 21:16:05 -0500

    add youtube component

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,33 @@
-import CMS from 'netlify-cms'
+import CMS from "netlify-cms";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import ProductPagePreview from "./preview-templates/ProductPagePreview";
 
-CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewStyle("/styles.css");
+CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("products", ProductPagePreview);
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
+
+// CMS.registerEditorComponent("pullQuote", PullQuoteWidget, PullQuotePreview);
+CMS.registerEditorComponent({
+  id: "youtube",
+  label: "Youtube",
+  fields: [{ name: "id", label: "Youtube Video ID", widget: "string" }],
+  pattern: /^youtube (\S+)$/,
+  fromBlock: function(match) {
+    return {
+      id: match[1]
+    };
+  },
+  toBlock: function(obj) {
+    return "youtube " + obj.id;
+  },
+  toPreview: function(obj) {
+    return (
+      '<img src="http://img.youtube.com/vi/' +
+      obj.id +
+      '/maxresdefault.jpg" alt="Youtube Video"/>'
+    );
+  }
+});
---
+++ src/cms/preview-templates/PullQuotePreview.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const PullQuotePreview = () => <p>Pullquote preview</p>;
+
+PullQuotePreview.propTypes = {};
+
+export default PullQuotePreview;
